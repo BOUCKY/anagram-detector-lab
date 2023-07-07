fa995ec866b53d6da01887ab0e0b07cacaaa80fa
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-07-07 09:26:37 -0400

    Complete Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_of_words):
+        anagrams = []
+        sorted_word = sorted(self.word)
+
+        for w in list_of_words:
+            if w.lower() != self.word and sorted(w.lower()) == sorted_word:
+                anagrams.append(w)
+
+        return anagrams
